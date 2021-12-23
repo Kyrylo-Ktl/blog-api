ddf562401f1a815e427a341d4a8b1c45e02b8e19
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2021-12-23 18:08:47 +0200

    Restricted access to editing posts and comments only for the author or administrator

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -9,12 +9,19 @@ from .serializers import UserSerializer
 
 
 class UserCreateView(CreateAPIView):
+    """
+    Allows you to register using your email nickname and password
+    """
     model = User
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
 
 class UserDetailView(RetrieveUpdateDestroyAPIView):
+    """
+    Profile view, displays information about the current user.
+    Only for authorized users
+    """
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
 
