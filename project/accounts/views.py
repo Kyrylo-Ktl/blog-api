644af9db90a375c071b09425b21668789f5aa1e5
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2021-12-23 16:18:13 +0200

    Added views and routes

--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+"""Module for describing model views"""
 
-# Create your views here.
+from django.contrib.auth.models import User
+from rest_framework.generics import CreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from .serializers import UserSerializer
+
+
+class UserCreateView(CreateAPIView):
+    model = User
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(RetrieveUpdateDestroyAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
