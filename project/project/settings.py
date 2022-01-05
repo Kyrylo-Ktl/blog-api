4b378741f2aab465843e062f6b4243af3944d32e
Kyrylo-Ktl <kyrylo.ktl@gmail.com>
2022-01-05 12:15:47 +0200

    Added lost migrations and postgres setting

--- project/project/settings.py
+++ project/project/settings.py
@@ -91,28 +91,28 @@ AUTH_USER_MODEL = 'accounts.User'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': env('SQL_ENGINE', 'django.db.backends.sqlite3'),
-#         'NAME': env('SQL_DATABASE', BASE_DIR / 'db.sqlite3'),
-#         'USER': env('SQL_USER', 'user'),
-#         'PASSWORD': env('SQL_PASSWORD', 'password'),
-#         'HOST': env('SQL_HOST', 'localhost'),
-#         'PORT': env('SQL_PORT', 5432),
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        'USER': 'user',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': 5432,
+        'ENGINE': env('SQL_ENGINE', 'django.db.backends.sqlite3'),
+        'NAME': env('SQL_DATABASE', BASE_DIR / 'db.sqlite3'),
+        'USER': env('SQL_USER', 'user'),
+        'PASSWORD': env('SQL_PASSWORD', 'password'),
+        'HOST': env('SQL_HOST', 'localhost'),
+        'PORT': env('SQL_PORT', 5432),
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#         'USER': 'user',
+#         'PASSWORD': 'password',
+#         'HOST': 'localhost',
+#         'PORT': 5432,
+#     }
+# }
 
 
 # Password validation
